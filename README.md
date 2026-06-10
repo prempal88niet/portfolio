8bdaf6be5424cea238da4cd74b56e9c54f745ed1
aartibaghel456 <aartibaghel456@gmail.com>
2026-06-10 15:18:09 +0530

    my portfolio

---
+++ README.md
@@ -0,0 +1,9 @@
+
+# GitHub Portfolio
+
+React + Vite Portfolio Template
+
+## Run
+
+npm install
+npm run dev
---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+
+export default function App() {
+  return (
+    <div style={{fontFamily:'Arial',padding:'40px'}}>
+      <h1>Prempal Singh</h1>
+      <h2>MERN/MEAN Stack Developer | Solution Architect</h2>
+
+      <h3>About Me</h3>
+      <p>
+        Experienced in React,Angular Node.js, MongoDB, PostgreSQL,
+        Microservices, AWS and System Design.
+      </p>
+
+      <h3>Skills</h3>
+      <ul>
+        <li>React</li>
+		<li>Angular</li>
+        <li>Node.js</li>
+        <li>MongoDB</li>
+        <li>PostgreSQL</li>
+        <li>AWS</li>
+        <li>Microservices</li>
+      </ul>
+
+      <h3>Projects</h3>
+      <ul>
+        <li>Daycare SaaS Platform</li>
+        <li>WhatsApp Automation Platform</li>
+        <li>Real-Time Chat System</li>
+      </ul>
+
+      <h3>Contact</h3>
+      <p>Email: prempal88niet@gmail.com</p>
+    </div>
+  )
+}
